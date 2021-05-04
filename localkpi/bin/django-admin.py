bdf67880eb5456c23b4effe1c6ec5aff65cf4a97
sondeche-rns <stephen.ondeche@reachnetworks.net>
2021-05-04 16:39:04 +0300

    chore(cleanup): delete localkpi environment

--- localkpi/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/media/od/527572B33774B8B1/Projects/REACH/nexus forms web/web inplimentation/nexusforms-kpi/localkpi/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
